4dbf167db49e84478728d71e134072a90a57e80f
Fan Yang <missdeer@dfordsoft.com>
2015-06-02 14:07:26 +0800

    (*)extrace friendship functions

---
+++ friendship.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func GetFriendsList() ([]UserProfile, error) {
+	var cursor int64 = -1
+	var users []UserProfile
+	for {
+		query := url.Values{}
+		query.Set("screen_name", user.ScreenName())
+		query.Set("user_id", fmt.Sprintf("%d", user.Id()))
+		query.Set("cursor", fmt.Sprintf("%d", cursor))
+		query.Set("count", "2000")
+		req, err = http.NewRequest("GET", fmt.Sprintf("/1.1/friends/list.json?%v", query.Encode()), nil)
+		if err != nil {
+			fmt.Printf("Could not parse request: %v\n", err)
+			return nil, err
+		}
+
+		resp, err = client.SendRequest(req)
+		if err != nil {
+			fmt.Printf("Could not send request: %v\n", err)
+			return nil, err
+		}
+		userColl := new(UserCollection)
+
+		if b, err := ReadBody(resp); err != nil {
+			return nil, err
+		} else {
+			err = json.Unmarshal(b, userColl)
+			if err == io.EOF {
+				err = nil
+			}
+			if err != nil {
+				fmt.Printf("Problem parsing friends response: %v\n", err)
+				return nil, err
+			}
+		}
+
+		if len(userColl.Users) < 1 {
+			break
+		}
+		users = append(users, userColl.Users...)
+		cursor = userColl.NextCursor
+	}
+	return users, nil
+}
+
+func GetFollowersList() ([]UserProfile, error) {
+	/// get followers list
+	var cursor int64 = -1
+	var users []UserProfile
+	for {
+		query := url.Values{}
+		query.Set("screen_name", user.ScreenName())
+		query.Set("user_id", fmt.Sprintf("%d", user.Id()))
+		query.Set("cursor", fmt.Sprintf("%d", cursor))
+		query.Set("count", "2000")
+		req, err = http.NewRequest("GET", fmt.Sprintf("/1.1/followers/list.json?%v", query.Encode()), nil)
+		if err != nil {
+			fmt.Printf("Could not parse request: %v\n", err)
+			return nil, err
+		}
+
+		resp, err = client.SendRequest(req)
+		if err != nil {
+			fmt.Printf("Could not send request: %v\n", err)
+			return nil, err
+		}
+		userColl := new(UserCollection)
+
+		if b, err := ReadBody(resp); err != nil {
+			return nil, err
+		} else {
+			err = json.Unmarshal(b, userColl)
+			if err == io.EOF {
+				err = nil
+			}
+			if err != nil {
+				fmt.Printf("Problem parsing followers response: %v\n", err)
+				return nil, err
+			}
+		}
+
+		if len(userColl.Users) < 1 {
+			break
+		}
+		users = append(users, userColl.Users...)
+		cursor = userColl.NextCursor
+	}
+	return users, nil
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"compress/gzip"
-	"encoding/json"
 	"fmt"
 	"github.com/kurrik/oauth1a"
 	"github.com/kurrik/twittergo"
@@ -10,7 +9,6 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 	"strings"
 )
@@ -131,95 +129,6 @@ func Authorize(force_auth bool) {
 	fmt.Printf("ID: %v\n", user.Id())
 	fmt.Printf("Name: %v\n", user.Name())
 	fmt.Printf("ScreenName: %v\n", user.ScreenName())
-}
-
-func GetFriendsList() ([]UserProfile, error) {
-	var cursor int64 = -1
-	var users []UserProfile
-	for {
-		query := url.Values{}
-		query.Set("screen_name", user.ScreenName())
-		query.Set("user_id", fmt.Sprintf("%d", user.Id()))
-		query.Set("cursor", fmt.Sprintf("%d", cursor))
-		query.Set("count", "2000")
-		req, err = http.NewRequest("GET", fmt.Sprintf("/1.1/friends/list.json?%v", query.Encode()), nil)
-		if err != nil {
-			fmt.Printf("Could not parse request: %v\n", err)
-			return nil, err
-		}
-
-		resp, err = client.SendRequest(req)
-		if err != nil {
-			fmt.Printf("Could not send request: %v\n", err)
-			return nil, err
-		}
-		userColl := new(UserCollection)
-
-		if b, err := ReadBody(resp); err != nil {
-			return nil, err
-		} else {
-			err = json.Unmarshal(b, userColl)
-			if err == io.EOF {
-				err = nil
-			}
-			if err != nil {
-				fmt.Printf("Problem parsing friends response: %v\n", err)
-				return nil, err
-			}
-		}
-
-		if len(userColl.Users) < 1 {
-			break
-		}
-		users = append(users, userColl.Users...)
-		cursor = userColl.NextCursor
-	}
-	return users, nil
-}
-
-func GetFollowersList() ([]UserProfile, error) {
-	/// get followers list
-	var cursor int64 = -1
-	var users []UserProfile
-	for {
-		query := url.Values{}
-		query.Set("screen_name", user.ScreenName())
-		query.Set("user_id", fmt.Sprintf("%d", user.Id()))
-		query.Set("cursor", fmt.Sprintf("%d", cursor))
-		query.Set("count", "2000")
-		req, err = http.NewRequest("GET", fmt.Sprintf("/1.1/followers/list.json?%v", query.Encode()), nil)
-		if err != nil {
-			fmt.Printf("Could not parse request: %v\n", err)
-			return nil, err
-		}
-
-		resp, err = client.SendRequest(req)
-		if err != nil {
-			fmt.Printf("Could not send request: %v\n", err)
-			return nil, err
-		}
-		userColl := new(UserCollection)
-
-		if b, err := ReadBody(resp); err != nil {
-			return nil, err
-		} else {
-			err = json.Unmarshal(b, userColl)
-			if err == io.EOF {
-				err = nil
-			}
-			if err != nil {
-				fmt.Printf("Problem parsing followers response: %v\n", err)
-				return nil, err
-			}
-		}
-
-		if len(userColl.Users) < 1 {
-			break
-		}
-		users = append(users, userColl.Users...)
-		cursor = userColl.NextCursor
-	}
-	return users, nil
 }
 
 func main() {
